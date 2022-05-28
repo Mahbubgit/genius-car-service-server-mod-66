65a34545110aa12f469d740ff779b699b48b2e80
Your Name <you@example.com>
2022-05-28 15:47:58 +0600

    step added

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ *  One Time for your computer
+ * -----------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ---------------------------
+ * For each project one time
+ * ---------------------------
+ * 1. heroku create
+ * 2. make sure you: git add ., git commit and git push
+ * 3. git push heroku main
+ */
